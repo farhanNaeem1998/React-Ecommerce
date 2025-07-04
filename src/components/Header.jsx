9bff82ac37291f781180513544a3ffd943014454
farhan1998 <farhan.khan1836@gmail.com>
2025-07-04 17:02:23 +0500

    Made the Header Responsive for mobile screen...

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,16 +5,17 @@ import heart from "../assets/Vector.png";
 
 export const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [searchClicked, setSearchClicked] = useState(false);
   return (
     <>
       <div className="w-full bg-white">
-        <div className="flex lg:justify-between md:justify-start items-center mx-50 md:mx-22 pt-4 pb-4 text-sm">
-          <div className="lg:ml-30 md:ml:14">
-            <p className="font-['Inter'] font-bold text-[24px] leading-[24px] tracking-[0.72px]">
+        <div className="flex justify-between lg:justify-between md:justify-start items-center mx-5 md:mx-22 pt-4 pb-4 text-sm ">
+          <div className="lg:ml-30 md:ml-14">
+            <p className="font-['Inter'] font-bold text-[20px] leading-[24px] tracking-[0.72px]">
               Exclusive
             </p>
           </div>
-          <div className="md:hidden lg:flex">
+          <div className="md:hidden lg:flex hidden">
             <ul className="flex gap-15 ">
               <li className="font-['Poppins'] font-normal text-[16px] leading-[24px] text-center">
                 Home
@@ -31,19 +32,25 @@ export const Header = () => {
             </ul>
           </div>
           <div className="flex items-center lg:justify-center lg:gap-5 lg:w-[400px] md:w-[1000px] md:justify-between md:gap-2 md:ml-22 lg:mr-20">
-            <div className="flex items-center gap-2 bg-gray-100 w-60 h-8 p-5">
+            <div className="hidden lg:flex md:flex items-center gap-2 bg-gray-100 w-60 h-8 p-5">
               <input type="text" placeholder="What are you looking for?" />
               <img src={search} alt="" className="w-6 h-6" />
             </div>
-            <div className="flex gap-5 items-center flex-row">
-              <div>
+            <div
+              className="flex gap-5 items-center flex-row"
+              onClick={() => setSearchClicked(!searchClicked)}
+            >
+              <div className="flex lg:hidden md:hidden">
+                <img src={search} alt="" className="w-8 h-8" />
+              </div>
+              <div className=" lg:flex md:flex">
                 <img src={heart} alt="" className="w-6 h-6" />
               </div>
-              <div>
+              <div className=" lg:flex md:flex">
                 <img src={cart} alt="" className="w-7 h-7" />
               </div>
               <div
-                className="lg:hidden md:flex"
+                className="flex lg:hidden md:flex"
                 onClick={() => setIsOpen(!isOpen)}
               >
                 <img
@@ -75,6 +82,21 @@ export const Header = () => {
             </div>
           )}
         </div>
+        {searchClicked && (
+          <div className="absolute top-29 left-1/2 transform -translate-x-1/2 bg-gray-100 w-[300px] h-[40px] flex items-center justify-between px-4 shadow-lg rounded-lg z-50">
+            <input
+              type="text"
+              placeholder="What are you looking for?"
+              className="w-full h-full bg-transparent outline-none"
+            />
+            <img
+              src={search}
+              alt=""
+              className="w-6 h-6"
+              onClick={() => setSearchClicked(false)}
+            />
+          </div>
+        )}
 
         <hr />
       </div>
