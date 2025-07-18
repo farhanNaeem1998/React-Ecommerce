40888245bb751a3cf5f6aa2d43719c5992a2a6f4
farhan1998 <farhan.khan1836@gmail.com>
2025-07-18 12:26:50 +0500

    Added some data in the footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,14 @@ import React from "react";
 export const Footer = () => {
   return (
     <>
-      <div className=" footer w-full bg-black">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+      <div className=" footer w-full bg-black text-white lg:py-6">
+        <div className="flex lg:flex-row lg:mx-50 lg:items-center lg:justify-between">
+          <div>Exclusive</div>
+          <div>Support</div>
+          <div>Account</div>
+          <div>Quick Link</div>
+          <div>Download App</div>
+        </div>
       </div>
       ;
     </>
