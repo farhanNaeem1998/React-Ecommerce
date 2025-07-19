66f66c9a9bd9b084eec7dc6dcb353ac85cd076f7
farhan1998 <farhan.khan1836@gmail.com>
2025-07-19 23:53:53 +0500

    Changed the display property of the footer elements to flex column

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,11 +5,11 @@ export const Footer = () => {
     <>
       <div className=" footer w-full bg-black text-white lg:py-6">
         <div className="flex lg:flex-row lg:mx-50 lg:items-center lg:justify-between">
-          <div>Exclusive</div>
-          <div>Support</div>
-          <div>Account</div>
-          <div>Quick Link</div>
-          <div>Download App</div>
+          <div className="flex flex-col">Exclusive</div>
+          <div className="flex flex-col">Support</div>
+          <div className="flex flex-col">Account</div>
+          <div className="flex flex-col">Quick Link</div>
+          <div className="flex flex-col">Download App</div>
         </div>
       </div>
       ;
