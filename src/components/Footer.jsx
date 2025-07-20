ca5530e859c97676eb0d2a5232090f8ccb4d31cf
farhan1998 <farhan.khan1836@gmail.com>
2025-07-20 17:29:52 +0500

    Added multiple divs inside the each flex-column item in the footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,11 +5,39 @@ export const Footer = () => {
     <>
       <div className=" footer w-full bg-black text-white lg:py-6">
         <div className="flex lg:flex-row lg:mx-50 lg:items-center lg:justify-between">
-          <div className="flex flex-col">Exclusive</div>
-          <div className="flex flex-col">Support</div>
-          <div className="flex flex-col">Account</div>
-          <div className="flex flex-col">Quick Link</div>
-          <div className="flex flex-col">Download App</div>
+          <div className="flex flex-col">
+            <div>Exclusive</div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <div className="flex flex-col">
+            <div>Support</div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <div className="flex flex-col">
+            <div>Account</div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <div className="flex flex-col">
+            <div>Quick Link</div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <div className="flex flex-col">
+            <div>Download App</div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
         </div>
       </div>
       ;
